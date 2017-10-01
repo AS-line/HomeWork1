8c22e2594d4f7803a988dec76aea3f3c8ede70de
As-line <32175896+AS-line@users.noreply.github.com>
2017-10-01 14:40:13 +0300

    Update home2.py

--- home2.py
+++ home2.py
@@ -45,15 +45,39 @@ print('далее...')
 time.sleep(1)
 print('Шестой вопрос--->Какой самый популярный формат для хранения растровых изображений.')
 q6 = str(input("Ответ: "))
-if q6 == 'JPEG' or 'JPG':
+if q6 == 'JPEG' or 'JPG' or 'jpg' or 'jpeg':
 	score +=1
 else:
 	score -=1
 print('далее...')
 time.sleep(1)
-print('Седьмой вопрос--->Какой пароль от ноутбуков PERI?')
-q7 = int(input('Ответ: '))
-if q7 == 311635:
+print('Седьмой вопрос--->Как передавались данные между компьютерами до появления сети Интернет?')
+q7 = str(input('Ответ: '))
+if q7 == 'вручную' or 'Вручную':
+	score +=1
+else:
+	score -=1
+print('далее...')
+time.sleep(1)
+print('Восьмой вопрос--->Когда отмечают Международный день блога?')
+q8 = str(input('Ответ: '))
+if q8 == '31 Августа' or '31 августа':
+	score +=1
+else:
+	score -=1
+print('далее...')
+time.sleep(1)
+print('Девятый вопрос--->Какой самый популярный поисковик в мире?')
+q9 = str(input('Ответ: '))
+if q9 == 'Google' or 'google':
+	score +=1
+else:
+	score -=1
+print('далее...')
+time.sleep(1)
+print('Десятый вопрос--->Первый развлекательный сайт в России')
+q10 = str(input('Ответ: '))
+if q10 == 'anekdot.ru' or 'Anekdot.ru' or 'Анекдот.ру':
 	score +=1
 else:
 	score -=1
@@ -61,9 +85,9 @@ input('Конец. Нажмите ENTER для подведения счета')
 print('--->PLEASE WAIT<---')
 time.sleep(3)
 print('Ваш счет: ', score)
-if score <= 2 :
+if score <= 5 :
 	print('Попробуйте подучить теорию')
-if 5 > score < 3 :
+if 8 >= score <= 6 :
 	print('Средний результат')
-if score > 6 :
+if score >= 9 :
 	print('Поздравляем, вы прошли тест')
